368610fb7ba65cfb01dee8af6dda8d2bb097672a
Henr1queSantos <43189446+Henr1queSantos@users.noreply.github.com>
2025-06-02 14:15:20 +0100

    PORT

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
